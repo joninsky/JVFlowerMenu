20b90da586b7c940a9fa319fc8dda0ea6599be35
Jon Vogel <joninsky@gmail.com>
2017-01-17 16:10:32 -0800

    With Framework

---
+++ JVFlowerMenu/JVFlowerMenu/JVFlowerMenu.h
@@ -0,0 +1,19 @@
+//
+//  JVFlowerMenu.h
+//  JVFlowerMenu
+//
+//  Created by Jon Vogel on 1/17/17.
+//  Copyright © 2017 Jon Vogel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JVFlowerMenu.
+FOUNDATION_EXPORT double JVFlowerMenuVersionNumber;
+
+//! Project version string for JVFlowerMenu.
+FOUNDATION_EXPORT const unsigned char JVFlowerMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JVFlowerMenu/PublicHeader.h>
+
+
---
+++ JVFlowerMenu/JVFlowerMenu/JVFlowerMenu.swift
@@ -0,0 +1,9 @@
+//
+//  JVFlowerMenu.swift
+//  JVFlowerMenu
+//
+//  Created by Jon Vogel on 1/17/17.
+//  Copyright © 2017 Jon Vogel. All rights reserved.
+//
+
+import Foundation
